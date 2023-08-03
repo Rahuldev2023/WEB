3e9f5d39121e4983829471095177c5fa1cac93ee
Rahuldev2023 <ghoshrahul1018@gmail.com>
2023-08-03 19:24:38 +0530

    new project

--- NewsLetter-SignUp/app.js
+++ NewsLetter-SignUp/app.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 const request=require("request");
+const https=require("https");
 
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -12,13 +13,58 @@ app.get("/",function(req,res){
 });
 
 app.post("/",function(req,res){
-var firstname=req.body.fn;
-var lastname=req.body.ln;
-var email=req.body.e;
+    const firstname=req.body.fn;
+    const lastname=req.body.ln;
+    const email=req.body.e;
 
-console.log(firstname,lastname,email);
+    const data={
+        //data objects
+        members:[ {
+                email_address:email,
+                status:"subscribed",
+                merge_fields:{
+                    FNAME:firstname,
+                    LNAME:lastname
+                }
+
+            }
+        ]
+    };
+        const jsonData=JSON.stringify(data);//object to string
+        const url="https://us21.api.mailchimp.com/3.0/lists/6b4699bcd3";
+
+        const options={
+            method:"POST",
+            auth:"rahul23:c4b800520340528fd2fd421b401b055f-us21"
+        };
+
+        const request=https.request(url,options,function(response){
+           if(response.statusCode === 200){
+                res.sendFile(__dirname+"/success.html");
+           }
+           else{
+            res.sendFile(__dirname+"/failure.html");
+           }
+            response.on("data",function(data){
+                console.log(JSON.parse(data));
+            });
+
+             });
+
+            request.write(jsonData);
+            request.end();
+
+});
+
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
 });
 
 app.listen(3000,function(){
     console.log("Server Running.......");
 });
+
+
+//api-key:c4b800520340528fd2fd421b401b055f-us21
+//audience list-id:6b4699bcd3.
